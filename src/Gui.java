8d4d470fa752f7582fb52a68fc92339885c7dbef
Alex Merder <cskachampi@gmail.com>
2020-09-25 19:50:45 +0300

    тест

--- src/Gui.java
+++ src/Gui.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Gui {
 
 
     public static void main(String[] args) {
-
+//создаем форму и указываем параметры
         JFrame frame = new JFrame("Phonebook");
-        
+        frame.setSize(1000,600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBackground(Color.BLACK);
+
+
+
+
+        frame.setVisible(true);
 
 
 
