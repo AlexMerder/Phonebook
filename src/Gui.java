ae80b787e05cf4f4e338fd3bc9fcc202815f7408
Alex Merder <cskachampi@gmail.com>
2020-09-26 22:02:32 +0300

    Расположил кнопки на форму входа

--- src/Gui.java
+++ src/Gui.java
@@ -6,33 +6,38 @@ public class Gui {
 
     public static void main(String[] args) {
 //создаем форму и указываем параметры
-        JFrame frame = new JFrame("Phonebook - Sigh in");
-        frame.setSize(1000,600);
+        JFrame frame = new JFrame("Phonebook");
+        frame.setSize(300, 150);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-
         JPanel panel = new JPanel();
-        frame.add(panel,BorderLayout.CENTER);
+        panel.setLayout(new GridBagLayout());
+        frame.add(panel, BorderLayout.CENTER);
+
+        panel.setBackground(Color.LIGHT_GRAY);
+
+        JTextField loginText = new JTextField(15);
+        JPasswordField passwordField = new JPasswordField(15);
+        JLabel loginLabel = new JLabel("LOGIN");
+        JLabel passwordLabel = new JLabel("PASSWORD");
+        JButton buttonSighIn = new JButton("Sigh in");
+        JButton buttonRegistration = new JButton("Registration");
 
 
+        panel.add(loginLabel, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(10, 0, 0, 0), 0, 0));
+        panel.add(passwordLabel, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
 
-        JTextField text = new JTextField("Login",10);
-        JTextField text2 = new JTextField("Password",10);
+        panel.add(loginText, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(1, 1, 1, 1), 0, 0));
+        panel.add(passwordField, new GridBagConstraints(1, 1, 1, 1, 0.0, 00.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(1, 1, 1, 1), 0, 0));
 
-        panel.add(text,BorderLayout.CENTER);
-        panel.add(text2,BorderLayout.SOUTH);
-
-        text.setBackground(Color.BLUE);
-        text2.setBackground(Color.BLUE);
-
+        panel.add(buttonSighIn, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(1, 1, 1, 1), 0, 0));
+        panel.add(buttonRegistration, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(1, 1, 1, 1), 0, 0));
 
 
-
+        frame.setResizable(false);
         frame.setVisible(true);
-
-
 
 
     }
